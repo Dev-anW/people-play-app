90a85f4a66b16f94802457229f2628b766a051f8
Dev-anW <wollenschlaegerd@gmail.com>
2025-09-10 08:51:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Use envsubst to replace the ${PORT} variable in the template
 # and create the final nginx configuration file.
-envsubst < /etc/nginx/http.d/nginx.conf.template > /etc/nginx/http.d/default.conf
+envsubst '${PORT}' < /etc/nginx/http.d/nginx.conf.template > /etc/nginx/http.d/default.conf
 
 # Start PHP-FPM in the background
 php-fpm &
