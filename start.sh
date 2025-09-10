71d0f8b84c0b2678b218500f9ecc4dda11113c6f
Dev-anW <wollenschlaegerd@gmail.com>
2025-09-10 08:45:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+# Use envsubst to replace the ${PORT} variable in the template
+# and create the final nginx configuration file.
+envsubst < /etc/nginx/http.d/nginx.conf.template > /etc/nginx/http.d/default.conf
+
 # Start PHP-FPM in the background
 php-fpm &
+
 # Start Nginx in the foreground
 nginx -g "daemon off;"
