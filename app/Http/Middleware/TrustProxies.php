3b8160bf835586506a7573defce6be0ede34d6a2
Dev-anW <wollenschlaegerd@gmail.com>
2025-09-10 10:47:32 +0200

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // THIS IS THE FIX: We are telling Laravel to trust any proxy.
+    // This is safe and necessary for platforms like Render.
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
