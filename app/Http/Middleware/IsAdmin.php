a279b9e67d5fed18e549c88d2af215d561971abb
Dev-anW <wollenschlaegerd@gmail.com>
2025-09-10 14:14:49 +0200

    Update IsAdmin.php

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-// 1. IMPORT THE CORRECT RESPONSE CLASS
 use Symfony\Component\HttpFoundation\Response;
 
 class IsAdmin
@@ -14,15 +13,13 @@ class IsAdmin
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    // 2. UPDATE THE RETURN TYPE HINT
     public function handle(Request $request, Closure $next): Response
     {
         if (auth()->check() && auth()->user()->is_admin) {
             return $next($request);
         }
 
-        // Now, this RedirectResponse is a valid return type because it's a
-        // child of the Symfony Response class we imported.
+       
         return redirect('/home');
     }
 }
