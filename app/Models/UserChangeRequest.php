4bab374a99e6f0e32dd6ed2ec13013cca6665bce
Dev-anW <wollenschlaegerd@gmail.com>
2025-09-10 14:18:04 +0200

    Update UserChangeRequest.php

--- app/Models/UserChangeRequest.php
+++ app/Models/UserChangeRequest.php
@@ -17,18 +17,18 @@ class UserChangeRequest extends Model
         'reviewed_at',
     ];
 
-    // Cast the requested_data attribute from JSON to an array automatically
+    
     protected $casts = [
     'requested_data' => 'array',
     'reviewed_at' => 'datetime', // Add this line
 ];
-    // A request belongs to a user
+    
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // A request was reviewed by an admin (another user)
+    
     public function reviewer()
     {
         return $this->belongsTo(User::class, 'reviewed_by');
